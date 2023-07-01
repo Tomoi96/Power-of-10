3e8d70b940969c2c11b9d1766b7ac15783cc686a
Tomoi96 <tomislav.lazaret@gmail.com>
2023-07-01 19:38:56 +0200

    output

--- power_of_10.ps1
+++ power_of_10.ps1
@@ -9,3 +9,6 @@ $final_number -= 15
 $final_number /= $original_number
 $final_number += 7
 $number_is_10 = $final_number -eq 10
+Write-Host "The number is equal to 10; $number_is_10"
+Write-Host "The final number is $final_number"
+Write-Host "You wrote $original_number"
