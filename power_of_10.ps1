5610db0fb74fa4af0b5efa318bf43c739b037adb
Tomoi96 <tomislav.lazaret@gmail.com>
2023-07-01 19:36:10 +0200

    number requirement

--- power_of_10.ps1
+++ power_of_10.ps1
@@ -1,3 +1,4 @@
 $Env:USER = "Sir"
 Write-Host "Greetings, $Env:USER!" 
 Write-Host "Whatever number you start with, I will morph it into 10 through the powers of mathematics!`n"
+[int]$original_number = Read-Host -Prompt "Type a number, any number"
